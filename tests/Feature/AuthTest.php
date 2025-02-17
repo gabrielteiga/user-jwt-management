2b46201f739279304db8b944f6ffaccb2ab2eef0
Gabriel Teiga <gabriel.teiga.uappi@gmail.com>
2025-02-17 00:57:45 -0300

    feat: new auth unauthorized test with green validation

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -9,28 +9,40 @@ class AuthTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_example(): void
-    {
-        $userData = [
-            'name'          => 'gabriel test',
-            'email'         => 'teste@gabriel.com',
-            'password'      => 'testegabriel',
-            'cpf'           => '03303303394',
-            'phone_number'  => '55998765432'
-        ];
+    public array $userData = [
+        'name'          => 'gabriel test',
+        'email'         => 'teste@gabriel.com',
+        'password'      => 'testegabriel',
+        'cpf'           => '03303303394',
+        'phone_number'  => '55998765432'
+    ];
 
-        $response = $this->postJson('api/user', $userData);
+    public function test_user_can_be_authenticated_after_created(): void
+    {
+        $response = $this->postJson('api/user', $this->userData);
 
         $response->assertStatus(201);
 
         $credentials = [
-            'email'     => $userData['email'],
-            'password'  => $userData['password']
+            'email'     => $this->userData['email'],
+            'password'  => $this->userData['password']
         ];
 
         $response = $this->postJson('api/auth', $credentials);
 
         $response->assertStatus(200)
             ->assertJsonStructure(['access_token']);
+    }
+
+    public function test_user_cannot_be_authenticated_before_created(): void
+    {
+        $credentials = [
+            'email'     => $this->userData['email'],
+            'password'  => $this->userData['password']
+        ];
+
+        $response = $this->postJson('api/auth', $credentials);
+
+        $response->assertStatus(401);
     }
 }
