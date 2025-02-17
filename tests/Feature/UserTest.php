535f4592730027557129f65cc107ba8cf6d4d34d
Gabriel Teiga <gabriel.teiga.uappi@gmail.com>
2025-02-17 01:08:04 -0300

    feat: created new red test to get data from authorized users

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -27,7 +27,7 @@ class UserTest extends TestCase
         ]);
     }
 
-    public function test_cannot_create_user_without_email_field():void
+    public function test_cannot_create_user_without_email_field(): void
     {
         $user = $this->userData;
         unset($user['email']);
@@ -35,5 +35,42 @@ class UserTest extends TestCase
         $response = $this->postJson('/api/user', $user);
 
         $response->assertStatus(400);
+    }
+
+    public function test_user_can_get_data_using_valid_token_validation(): void
+    {
+        $userData = $this->userData;
+        
+        $token = $this->createUserAndGetToken($userData);
+
+        $user = $this->getJson('api/user', ['Authorization' => "Bearer $token"]);
+
+        $user->assertOk()
+            ->assertJsonStructure([
+                'user' => [
+                    'id',
+                    'name',
+                    'email',
+                    'cpf',
+                    'phone_number',
+                    'addresses'
+                ]
+            ]);
+    }
+
+    private function createUserAndGetToken(array $userData): string
+    {
+        $user = $this->postJson('api/user', $userData);
+
+        $user->assertCreated();
+        
+        $token = $this->postJson('api/auth', [
+            'email'     => $userData['email'],
+            'password'  => $userData['password']
+        ]);
+
+        $token->assertOk();
+
+        return $token->json('access_token');
     }
 }
