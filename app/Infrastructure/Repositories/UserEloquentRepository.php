e36d87aca8e1060330638ae2bfc3c634dd8d57f9
Gabriel Teiga <gabriel.teiga.uappi@gmail.com>
2025-02-17 01:55:45 -0300

    refactor: creating method that creates entity user using a model user from eloquent

--- app/Infrastructure/Repositories/UserEloquentRepository.php
+++ app/Infrastructure/Repositories/UserEloquentRepository.php
@@ -17,19 +17,18 @@ class UserEloquentRepository implements UserRepositoryInterface {
             'phone_number'  => $user->phone_number
         ]);
 
-        return new User(
-            $userModel->name,
-            $userModel->email,
-            $userModel->password,
-            $userModel->cpf,
-            $userModel->phone_number
-        );
+        return $this->createUserfromEloquentModel($userModel);
     }
 
     public function findUserById(int $userId): ?User
     {
         $userModel = ModelsUser::with('address')->find($userId);
-        
+
+        return $this->createUserfromEloquentModel($userModel);
+    }
+
+    private function createUserfromEloquentModel(ModelsUser $userModel): User
+    {
         return new User(
             $userModel->name,
             $userModel->email,
